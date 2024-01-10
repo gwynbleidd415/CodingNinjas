c8cee06c6d7224ec30af0aa4da30746c03b9cdf3
Mayank Chandwasia <mayankchandwasia.work@gmail.com>
2024-01-10 19:53:56 +0530

    solution for subset-sum-equal-to-k

---
+++ subset-sum-equal-to-k/solution1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+
+bool solution1(int n, int k, vector<int> &arr) {
+    vector<vector<bool>> dp(n, vector<bool>(k+1));
+    for(int i{0};i<n;++i) dp[i][0] = true;
+    if(arr.front() <= k) dp[0][arr.front()] = true;
+    cout << '\n';
+    for(int i{1};i<n;++i) {
+        for(int j{1};j<=k;++j) {
+            dp[i][j] = arr[i] > j ? dp[i-1][j] : (dp[i-1][j] || dp[i-1][j-arr[i]]);
+            if(arr[i] < j) continue;
+        }
+    }
+    return dp.back().back();
+}
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    // Write your code here.
+    return solution1(n, k, arr);
+}
