2a064627c72e304fd37e3e015656a98f4355b030
Mayank Chandwasia <mayankchandwasia.work@gmail.com>
2024-01-10 20:51:27 +0530

    solution for 0-1-knapsack

--- 0-1-knapsack/solution.cpp
+++ 0-1-knapsack/solution.cpp
@@ -9,14 +9,27 @@ int solution1(vector<int> weight, vector<int> value, int n, int maxWeight)
 	}
 	for(int i{1};i<n;++i) {
 		for(int j{1};j<=maxWeight;++j) {
-			int temp = max(dp[i-1][j], dp[i][j-1]);
-			dp[i][j] = (weight[i] > j) ? temp : max(temp, dp[i-1][j-weight[i]] + value[i]);
+			dp[i][j] = (weight[i] > j) ? dp[i-1][j] : max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]);
 		}
 	}
 	return dp.back().back();
 }
+int solution2(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	// Write your code here
+	vector<int> dp(maxWeight + 1);
+	for(int i{weight.front()};i<=maxWeight;++i) dp[i] = value.front();
+	for(int i{1};i<n;++i) {
+		for(int j{maxWeight};j>0;--j) {
+			dp[j] = (weight[i] > j) ? dp[j] : max(dp[j], dp[j-weight[i]] + value[i]);
+		}
+	}
+	return dp.back();
+}
+
 int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
 {
 	// Write your code here
-	return solution1(weight, value, n, maxWeight);
+	// return solution1(weight, value, n, maxWeight);
+	return solution2(weight, value, n, maxWeight);
 }
