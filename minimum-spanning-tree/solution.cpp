e4883c4908c80e07eb533035175d2c16ff477487
Mayank Chandwasia <mayankchandwasia.work@gmail.com>
2024-01-27 00:06:36 +0530

    solution for minimum-spanning-tree

--- minimum-spanning-tree/solution.cpp
+++ minimum-spanning-tree/solution.cpp
@@ -1,5 +1,29 @@
 #include <queue>
 
+class DisjointSet {
+	vector<int> par, sz;
+public:
+	DisjointSet(int n) {
+		par.resize(n), sz.resize(n);
+		iota(par.begin(), par.end(), 0);
+	}
+	int find(int i) {
+		if(par[i] == i) return i;
+		return par[i] = find(par[i]);
+	}
+	void unite(int i, int j) {
+		int pari{find(i)}, parj{find(j)};
+		if(pari == parj) return;
+		if(sz[parj] > sz[pari]) {
+			sz[parj] += sz[pari];
+			par[pari] = parj;
+		} else {
+			sz[pari] += sz[parj];
+			par[parj] = pari;
+		}
+	}
+};
+
 int solution1(int n, vector<vector<int>> &edges) {
 	vector<vector<pair<int, int>>> adj(n);
 	bool s1{true};
@@ -32,8 +56,23 @@ int solution1(int n, vector<vector<int>> &edges) {
 	return mst;
 }
 
+int solution2(int n, vector<vector<int>> &edges) {
+	DisjointSet dj(n+1);
+	sort(edges.begin(), edges.end(), [](vector<int> &a, vector<int> &b) {
+		return a[2] < b[2];
+	});
+	int mst{0};
+	for(vector<int> &edge:edges) {
+		if(dj.find(edge[0]) == dj.find(edge[1])) continue;
+		dj.unite(edge[0], edge[1]);
+		mst += edge[2];
+	}
+	return mst;
+}
+
 int kruskalMST(int n, vector<vector<int>> &edges)
 {
 	// Write your code here.
-	return solution1(n, edges);
+	// return solution1(n, edges);
+	return solution2(n, edges);
 }
