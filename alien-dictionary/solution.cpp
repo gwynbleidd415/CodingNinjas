bbcb958a7a11bbe598c9a0c3271205c729bdd396
Mayank Chandwasia <mayankchandwasia.work@gmail.com>
2024-01-26 20:05:47 +0530

    solution for alien-dictionary

--- alien-dictionary/solution.cpp
+++ alien-dictionary/solution.cpp
@@ -1,18 +1,5 @@
 #include <unordered_set>
 
-void solution1Helper(vector<string> &dictionary, vector<unordered_set<int>> &adj, int si, int ei, int ii) {
-    while(si<ei && dictionary[si].size() == ii) ++si;
-    int nsi{si}, nei{si};
-    for(int i{si+1};i<=ei;++i) {
-        if(dictionary[i][ii] != dictionary[nsi][ii]) {
-            adj[dictionary[nsi][ii] - 'a'].insert(dictionary[i][ii] - 'a');
-            solution1Helper(dictionary, adj, nsi, nei, ii+1);
-            nsi = i;
-        }
-        nei = i;
-    }
-}
-
 string topoSort(vector<unordered_set<int>> &adj, int k) {
     vector<int> inorder(k, 0);
     for(int i{0};i<k;++i) {
@@ -31,6 +18,19 @@ string topoSort(vector<unordered_set<int>> &adj, int k) {
     return str;
 }
 
+void solution1Helper(vector<string> &dictionary, vector<unordered_set<int>> &adj, int si, int ei, int ii) {
+    while(si<ei && dictionary[si].size() == ii) ++si;
+    int nsi{si}, nei{si};
+    for(int i{si+1};i<=ei;++i) {
+        if(dictionary[i][ii] != dictionary[nsi][ii]) {
+            adj[dictionary[nsi][ii] - 'a'].insert(dictionary[i][ii] - 'a');
+            solution1Helper(dictionary, adj, nsi, nei, ii+1);
+            nsi = i;
+        }
+        nei = i;
+    }
+}
+
 string solution1(vector<string> &dictionary, int k)
 {
     // Write your code here.
@@ -38,8 +38,26 @@ string solution1(vector<string> &dictionary, int k)
     solution1Helper(dictionary, adj, 0, dictionary.size()-1, 0);
     return topoSort(adj, k);
 }
+
+string solution2(vector<string> &dictionary, int k)
+{
+    // Write your code here.
+    vector<unordered_set<int>> adj(k);
+    int ei = dictionary.size()-1;
+    for(int i{0};i<ei;++i) {
+        for(int j{0};j<dictionary[i].size();++j) {
+            if(dictionary[i][j] != dictionary[i+1][j]) {
+                adj[dictionary[i][j]-'a'].insert(dictionary[i+1][j]-'a');
+                break;
+            }
+        }
+    }
+    return topoSort(adj, k);
+}
+
 string getAlienLanguage(vector<string> &dictionary, int k)
 {
     // Write your code here.
-    return solution1(dictionary, k);
+    // return solution1(dictionary, k);
+    return solution2(dictionary, k);
 }
